d4339b561e30ebc5609e00e64fbd5a6b21292caa
jukfloyd <zhuchkovav@gmail.com>
2022-07-28 00:36:03 +0300

    feat: test.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+	let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fithteen', 'sixteen', 'seventeen', 'eightteen', 'nineteen' ];
+	let dec = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let text = '';
+	if (number < 20) {
+		return  numbers[number-1];
+	} else if (number < 100) {
+		let fd = number.toString()[0];
+		let ld = number.toString()[1];
+		return (ld != '0') ? dec[fd-2] + ' ' + numbers[ld-1] : dec[fd-2];
+	} else {
+		let fd = number.toString()[0];
+		return (number%100 == 0) ? numbers[fd-1] + ' hundred' : numbers[fd-1] + ' hundred ' + toReadable(number - fd*100);
+	}
 }
