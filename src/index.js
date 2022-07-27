cb6f31315f88105b47827fadce206ff720dc26f2
jukfloyd <zhuchkovav@gmail.com>
2022-07-28 00:39:46 +0300

    fix: test.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = function toReadable (number) {
-	let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fithteen', 'sixteen', 'seventeen', 'eightteen', 'nineteen' ];
-	let dec = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+	let dec = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 	let text = '';
-	if (number < 20) {
+	if (number == 0) {
+		return 'zero'
+	} else if (number < 20) {
 		return  numbers[number-1];
 	} else if (number < 100) {
 		let fd = number.toString()[0];
